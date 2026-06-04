faaa2c4486d293e29db3f870b036d6af34053abc
Rohit Somreddi <yourgithub@email.com>
2026-06-04 20:49:28 +0530

    Fix Stripe.StripeConfig type error in build by using the exact dahlia API version literal type

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -6,7 +6,7 @@ const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 // and handle the fallback in the API endpoints to facilitate mock testing.
 export const stripe = stripeSecretKey
   ? new Stripe(stripeSecretKey, {
-      apiVersion: '2024-06-20' as unknown as Stripe.StripeConfig['apiVersion'],
+      apiVersion: '2024-06-20' as unknown as '2026-04-22.dahlia',
     })
   : null;
 
